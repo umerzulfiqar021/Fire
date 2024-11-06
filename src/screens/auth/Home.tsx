3c8bad7adb769579d24274b3c5b5777a67df5a73
umerzulfiqar021 <umerzulfiqar38@gmail.com>
2024-11-07 01:48:44 +0500

    changes

--- src/screens/auth/Home.tsx
+++ src/screens/auth/Home.tsx
@@ -1,17 +1,20 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import MainButton from '../../components/MainButton'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { loginAction } from '../../store/userActions'
 export default function Home({navigation}) {
 
 const isSignedIn = useSelector(state => state.userData.isSignedIn)
 const userName = useSelector(state => state.userData.userName)
-
+const dispatch = useDispatch()
   const handleClick = () =>{
+    dispatch(loginAction( ))
     navigation.navigate('Tabs')
+    
   }
   return (
-    <View style = {styles.container}>
+    <View style = {styles.container} className="mt-8 px-2">
       <Text>Home</Text>
 <MainButton  title='Go to Tabs' onPress={handleClick}/>
 {
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,8 @@
-import { combineReducers,createStore } from "redux";
+import { applyMiddleware, combineReducers,createStore } from "redux";
 import userReducer from "./userReducer";
 import AsyncStorage from '@react-native-async-storage/async-storage';
  import {persistStore,persistReducer} from 'redux-persist'
+import { thunk } from "redux-thunk";
 
 const persistConfig = {
     key: 'root',
@@ -12,7 +13,7 @@ const rootReducer = combineReducers({
     userData: persistReducer(persistConfig,userReducer)
 
 })
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer, applyMiddleware(thunk))
 export type RootState = ReturnType <typeof rootReducer>;
 export const persistor = persistStore(store)
 //this is for getting recommendations in the file we are using the data
