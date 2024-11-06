ddd63266f0db9ed54aa6390388f6af9850cdc12b
umerzulfiqar021 <umerzulfiqar38@gmail.com>
2024-11-06 22:35:58 +0500

    addedd async storage and used presisistore and persisiterreducer

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,18 @@
 import { combineReducers,createStore } from "redux";
 import userReducer from "./userReducer";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+ import {persistStore,persistReducer} from 'redux-persist'
 
+const persistConfig = {
+    key: 'root',
+   storage: AsyncStorage
+
+}
 const rootReducer = combineReducers({
-    userData: userReducer
+    userData: persistReducer(persistConfig,userReducer)
 
 })
 export const store = createStore(rootReducer)
 export type RootState = ReturnType <typeof rootReducer>;
+export const persistor = persistStore(store)
 //this is for getting recommendations in the file we are using the data
