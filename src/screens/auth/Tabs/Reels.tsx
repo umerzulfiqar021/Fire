da772d760cc669e49ae905ae3138a34e94dfcc0e
umerzulfiqar021 <umerzulfiqar38@gmail.com>
2024-11-13 00:26:23 +0500

    position absolute for button

--- src/screens/auth/Tabs/Reels.tsx
+++ src/screens/auth/Tabs/Reels.tsx
@@ -1,62 +1,49 @@
-import { StyleSheet, Text, View,TouchableOpacity, SafeAreaView } from 'react-native'
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import { logoutAction } from '../../../store/userActions';
+
 const Reels = () => {
-  const isSignedIn = useSelector(state => state.userData.isSignedIn)
-const userName = useSelector(state => state.userData.userName)
-const dispatch = useDispatch()
+  const isSignedIn = useSelector(state => state.userData.isSignedIn);
+  const userName = useSelector(state => state.userData.userName);
+  const dispatch = useDispatch();
+
   return (
-    
-
-    <>
-  
-       <View style={styles.logout}>
-      <TouchableOpacity  onPress={()=> dispatch(logoutAction())}>
-        <AntDesign name="logout" size={30} color='red' />
-
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.logout} onPress={() => dispatch(logoutAction())}>
+        <AntDesign name="logout" size={30} color="red" />
       </TouchableOpacity>
 
+      <Text>Reels</Text>
+
+      {isSignedIn ? (
+        <View>
+          <Text>The User is Signed In</Text>
+          <Text>Username: {userName}</Text>
+        </View>
+      ) : (
+        <View>
+          <Text>Please login first</Text>
+        </View>
+      )}
     </View>
-   
-    
-    <View style={styles.container}>
+  );
+};
 
-
-        <Text>Reels</Text>
-
-
-        {isSignedIn ? (
-          <View>
-            <Text> the User is Siged IN
-
-            </Text>
-            <Text> user name is {userName}</Text>
-          </View>
-        ) : (
-          <View>
-            <Text>Please login first {userName}</Text>
-          </View>
-        )}
-      </View></>
-  )
-}
-
-export default Reels
+export default Reels;
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
+  container: {
+    flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    backgroundColor: '#777',
   },
-  logout:{
-    
-    alignSelf: 'flex-end',
-    paddingRight: 5,
-    paddingTop: 5
-    
-  }
-
-})
+  logout: {
+    position: 'absolute',
+    top: 10,     // Distance from the top of the screen
+    right: 10,   // Distance from the right side of the screen
+    padding: 5,
+  },
+});
