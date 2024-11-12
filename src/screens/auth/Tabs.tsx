81f7d1d0f2947478f173b98d579499880ad6983e
umerzulfiqar021 <umerzulfiqar38@gmail.com>
2024-11-12 23:50:34 +0500

    addedd icons in tabs

--- src/screens/auth/Tabs.tsx
+++ src/screens/auth/Tabs.tsx
@@ -2,24 +2,44 @@ import {   StyleSheet } from 'react-native'
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import Profile from './Tabs/Profile'
-
+// import Icon from 'react-native-vector-icons/Fontisto';
+// import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 import Reels from './Tabs/Reels'
+
 import Tiktok from './Tabs/Tiktok'
+// import Icon from '@ant-design/icons';
 const Tabs = () => {
     const Tabs = createBottomTabNavigator()
   return (
    <Tabs.Navigator initialRouteName='Profile'
-   screenOptions={{}}
+   screenOptions={{tabBarShowLabel: false,
+      tabBarActiveTintColor: 'black',
+      tabBarInactiveTintColor: 'grey'
+
+
+   }}
    >
     <Tabs.Screen
     name='Profile'
     component={Profile}
-    options={{headerShown:false}}
+    options={{headerShown:false,
+      tabBarIcon : ({color,size}) => (
+        <AntDesign name="home" size={size} color= {color} />
+      )
+        
+      
+    }}
     />
     <Tabs.Screen
     name='Setting'
     component={Tiktok}
-    options={{headerShown:false}}
+    options={{headerShown:false,
+      tabBarIcon : ({color,size}) => (
+        <AntDesign name="setting" size={size} color= {color} />
+      )
+        
+    }}
     // options={{tabBarIcon: ()=>{
     //     <Image
     //     source={require('src\assets\icons\tik-tok.png')}
@@ -29,7 +49,12 @@ const Tabs = () => {
     <Tabs.Screen
     name='Reels'
     component={Reels}
-    options={{headerShown:false}}
+    options={{headerShown:false,
+      tabBarIcon : ({color,size}) => (
+        <AntDesign name="profile" size={size} color= {color} />
+      )
+        
+    }}
 
     />
    </Tabs.Navigator>
